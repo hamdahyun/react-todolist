623c323edf82ea7b830c63d28d2d3180a3580ca5
Ham Dahuyn <dahuyn1@gmail.com>
2021-07-18 18:28:22 +0900

    make video item

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-    <h1>{props.videos.snippet.title}</h1>
+const VideoItem = ({ video : {snippet} }) => (
+
+    // video전체를 전달하기 보다는 snippet을 나눠서 전달해도 될 것 같다고 생각.
+    <li className={styles.video}>
+        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+        <div className={styles.box}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+    </li>
 );
 
 export default VideoItem;
---
+++ youtube/src/components/video_search/video_search.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoSearch = (props) => (
+    <div>
+    </div>   
+    );
+
+export default VideoSearch;
